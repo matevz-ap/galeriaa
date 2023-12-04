ba2e207f9f8129b263dfef8d89c85ae691bfed2b
matevz-ap <matepeklaj@gmail.com>
2023-12-04 21:44:48 +0100

    Add home page

--- galleries/forms.py
+++ galleries/forms.py
@@ -5,6 +5,8 @@ from crispy_forms.layout import Submit
 from crispy_forms.layout import Field
 from crispy_forms.layout import Hidden
 from crispy_forms.layout import Layout
+from crispy_forms.layout import Div
+from crispy_forms.layout import HTML
 from . import models
 from . import utils
 
@@ -25,5 +27,9 @@ class GalleryForm(forms.ModelForm):
         self.helper.layout = Layout(
             Hidden("user", user_id),
             Field('folder'),
-            Submit('submit', 'Submit', css_class='w-100')
+            Div(
+                HTML('<a href="{% url "galleries:list" %}" class="btn btn-outline-secondary flex-grow-1">Close</a>'),
+                Submit('submit', 'Create', css_class='flex-grow-1'),
+                css_class='d-flex justify-content-end gap-3',
+            )
         )
