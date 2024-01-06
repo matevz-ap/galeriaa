5cd2fe1293b0a7fb85ce9d771e012576e9b47443
matevz-ap <matepeklaj@gmail.com>
2024-01-06 22:55:10 +0100

    Add spacing

--- galleries/models.py
+++ galleries/models.py
@@ -1,10 +1,19 @@
 import uuid
-from django.db import models
+
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Gallery(models.Model):
-    uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="galleries")
     folder = models.CharField(default="", max_length=33)
     data = models.JSONField(default=dict)
     name = models.CharField(default="", max_length=33)
+
+    @property
+    def spaced(self, cols=3) -> list[list[str]]:
+        s: list[list[str]] = [[], [], []]
+        for i, photo in enumerate(self.data):
+            s[i % cols].append(photo)
+        return s
