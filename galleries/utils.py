1084a9c25558bdac7e744fa27473f1f9cb7958d1
matevz-ap <matepeklaj@gmail.com>
2024-01-15 11:47:43 +0100

    wip

--- galleries/utils.py
+++ galleries/utils.py
@@ -2,9 +2,13 @@ def folders_to_choices(folders: list[dict[str, str]]) -> str:
     return "".join([f"<option value='{f['id']}'>{f['name']}</option>" for f in folders])
 
 
-def order_images(data: list[dict[str, str]], order: list[str]) -> list[dict[str, str]]:
-    order_dict: dict[str, int] = {}
-    for o in order:
-        image_id, order_num = o.split(":")
-        order_dict[image_id] = int(order_num)
-    return sorted(data, key=lambda x: order_dict[x["id"]])
+def order_images(data: list[dict[str, str]], order: list[str]) -> list[str]:
+    images: list[list[str]] = [[], [], []]
+    for i, photo in enumerate(order):
+        images[i % 3].append(photo)
+    img = []
+    for col in images:
+        for c in col:
+            img.append(c)
+    print(img)
+    return sorted(data, key=lambda x: img.index(x["id"]))
